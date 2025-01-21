1263032287cd03315900aba6115142ce6a833e79
luchunning <luchunning@inspirai.com>
2025-01-21 23:51:39 +0800

    update

--- client/tcpClient.py
+++ client/tcpClient.py
@@ -2,7 +2,8 @@ import socket
 
 tcp_client_socket = socket.socket()
 
-tcp_client_socket.connect(("127.0.0.1", 9999))
+# 将tcp_client_socket.connect设置为您要连接TCP Server的IP地址和Port端口
+tcp_client_socket.connect(("127.0.0.1", 9888))
 send_data = input(str("请输入数据："))
 
 tcp_client_socket.send(send_data.encode("utf-8"))
